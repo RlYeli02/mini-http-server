5b301bfdabe37646342e855acdca8d27c802b9b4
Franyeliza <franyelizarodriguez@live.com>
2021-03-18 00:45:22 -0400

    Server almost done

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 const PORT = 4000;
-var fs = require('fs');
+
 
 var messages = [
     {id:1, message:"Hola"},
@@ -12,45 +12,78 @@ var messages = [
 ]
 
 app.get('/', function (req, res){
-    let arr =[]
-    for(let k of messages){
-        arr.push(k.message)
+    try{
+        let arr =[]
+        for(let k of messages){
+            arr.push(k.message)
+        }
+        var result = arr.toString()
+        res.send(result)
+
+    }catch(err){
+        console.log(err)
     }
-    var result = arr.toString()
-    res.send(result)
 })
 
 // const data = {id:3, message:"klk"}
 
 app.post('/messages', async function(req, res){
-    var id= req.body;
-    var message=  req.body;
-    var re= await messages.push(id, message)
-    res.send("done")
-    console.log(messages)
+    try{
+
+        var id= req.body;
+        var message=  req.body;
+        var re= await messages.push(message)
+        console.log(messages)
+        res.send("done")
+    }
+    catch(err){
+        console.log(err)
+    }
+    
 
 })
 
 app.put('/messages/:id', function(req,res){
-    var id = req.params.id
-    
-    var Newmessage = req.body.message
-   
-    messages.map((x)=>{
-        if(x.id==id){
-            x.message = Newmessage;
-        }
-        return Newmessage
-    })
-    console.log("nuevo",Newmessage)
-    console.log(id)
-    res.send("done")
-    console.log(messages)
+    try{
+        var id = req.params.id
+        
+        var Newmessage = req.body.message
+       
+        messages.map((x)=>{
+            if(x.id==id){
+                x.message = Newmessage;
+            }
+            return Newmessage
+        })
+        console.log("nuevo",Newmessage)
+        console.log(id)
+        res.send("done")
+        console.log(messages)
+    }catch(err){
+        console.log(err)
+    }
     
 })
 
 
-app.delete('messages/id', function(req, res){
+app.delete('/messages/d/:id', function(req, res){
+    try{
+        var id = req.params.id
+
+         for( var i = 0; i < messages.length; i++){ 
+             console.log(messages[i].id)                   
+            if ( messages[i].id == id) { 
+                
+                messages.splice(i,1); 
+                
+            }
+        }
+        res.send("done")
+        console.log(messages)
+
+    }catch(err){
+        console.log(err)
+    }
 
 })
 
